0bda754f338ddfefc53e62572d811921e63aa8a5
Ninjaphil24 <mphilippos@hotmail.com>
2022-04-12 22:23:02 +0300

    Using js properly

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-console.log("Test")
+import _ from 'lodash';
+import './style.css';
+import Icon from './puddino.jpg'
+import Icon2 from './food.jpg'
+
+var content = document.getElementById('content');
+
+const navbar = () => {
+    const nav = document.createElement('nav');
+    const a1 = document.createElement('a');
+    const a2 = document.createElement('a');
+
+    a1.innerText = "Home ";
+    a2.innerText = " Recipe";
+
+    nav.appendChild(a1);
+    nav.appendChild(a2);
+    return nav;    
+}
+
+function homepage() {
+    const element = document.createElement('div');
+    
+    
+    element.classList.add('home');
+  
+    const myIcon = new Image();
+    myIcon.src = Icon2;
+
+    element.appendChild(myIcon);
+    return element;
+}
+
+content.appendChild(navbar());
+content.appendChild(homepage());
